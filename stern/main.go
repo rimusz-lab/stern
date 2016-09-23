06a1df425c738d37ddd24145ab71f2bcf2dc75f1
akupila <akupila@gmail.com>
2016-09-23 13:52:02 +0200

    wip tailing

--- stern/main.go
+++ stern/main.go
@@ -2,7 +2,9 @@ package stern
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/pkg/errors"
 	"github.com/wercker/stern/kubernetes"
@@ -21,19 +23,48 @@ func Run(ctx context.Context, config *Config) error {
 		return errors.Wrap(err, "failed to set up watch")
 	}
 
+	tails := make(map[string]*Tail)
+
 	go func() {
 		for p := range added {
-			log.Println("add", p.Pod, p.Container)
+			ID := id(p.Pod, p.Container)
+			if tails[ID] != nil {
+				// this shouldn't happen but in case it does anyway we'll ignore the dupe entry
+				continue
+			}
+
+			log.Printf("addded %s | %s\n", p.Pod, p.Container)
+
+			tail := NewTail(p.Pod, p.Container, &TailOptions{
+				Timestamps:   config.Timestamps,
+				SinceSeconds: config.Since,
+			})
+			tails[ID] = tail
+
+			done := tail.Start(ctx, input, os.Stdout)
+			
+			go func() {
+				<-done
+				tail.Close()
+				delete(tails[ID])
+			}
 		}
 	}()
 
 	go func() {
 		for p := range removed {
-			log.Println("remove", p.Pod, p.Container)
+			log.Printf("removed %s | %s\n", p.Pod, p.Container)
+			ID := id(p.Pod, p.Container)
+			tail := tails[ID]
+			tail.Close()
 		}
 	}()
 
 	<-ctx.Done()
 
 	return nil
+}
+
+func id(podID string, containerID string) string {
+	return fmt.Sprintf("%s-%s", podID, containerID)
 }
--- stern/tail.go
+++ stern/tail.go
@@ -1,1 +1,83 @@
 package stern
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/pkg/errors"
+
+	corev1 "k8s.io/client-go/1.4/kubernetes/typed/core/v1"
+	"k8s.io/client-go/1.4/pkg/api/v1"
+	"k8s.io/client-go/1.4/rest"
+)
+
+type Tail struct {
+	PodName       string
+	ContainerName string
+	Options       *TailOptions
+	req           *rest.Request
+	closed        chan bool
+}
+
+type TailOptions struct {
+	Timestamps   bool
+	SinceSeconds int64
+}
+
+func NewTail(podName, containerName string, options *TailOptions) *Tail {
+	return &Tail{PodName: podName, ContainerName: containerName, Options: options}
+}
+
+func (t *Tail) Start(ctx context.Context, i corev1.PodInterface, out io.Writer) chan error {
+	errc := make(chan error)
+
+	go func() {
+		req := i.GetLogs(t.PodName, &v1.PodLogOptions{
+			Follow:       true,
+			Timestamps:   t.Options.Timestamps,
+			Container:    t.ContainerName,
+			SinceSeconds: &t.Options.SinceSeconds,
+		})
+
+		readCloser, err := req.Stream()
+		if err != nil {
+			fmt.Println(errors.Wrap(err, "could not open stream"))
+		}
+		defer readCloser.Close()
+
+		stream, err := req.Stream()
+		if err != nil {
+			errc <- errors.Wrapf(err, "Error opening stream to %s: %s\n", t.PodName, t.ContainerName)
+		}
+
+		reader := bufio.NewReader(stream)
+
+		for {
+			line, err := reader.ReadBytes('\n')
+			if err != nil && err != io.EOF {
+				errc <- err
+				return
+			}
+
+			t.Print(string(line))
+		}
+	}()
+
+	go func() {
+		<-ctx.Done()
+		t.Close()
+	}()
+
+	return errc
+}
+
+func (t *Tail) Close() {
+	t.closed <- true
+}
+
+func (t *Tail) Print(msg string) {
+	log.Printf("%s %s | %s", t.PodName, t.ContainerName, msg)
+}
